a10a85c34610f2bcc5d28f245b29a051e7562135
Fernando Passos <30850453+fernandodmp@users.noreply.github.com>
2018-11-01 20:28:03 -0300

    Implements fit and predict functions

--- perceptron.py
+++ perceptron.py
@@ -1,15 +1,5 @@
 import numpy as np
 
-ts_inputs = np.array([[0,0,1,0],
-                      [1,1,1,0],
-                      [1,0,1,1],
-                      [0,1,1,1],
-                      [0,1,0,1],
-                      [1,1,1,1],
-                      [0,0,0,0]])
-
-ts_outputs = np.array([[0,1,1,0,0,1,0]]).T
-
 class Perceptron():
     def __init__(self):
         self.synapse_weights = np.random.rand(4,1)
@@ -18,7 +8,26 @@ class Perceptron():
         return 1/(1 + np.exp(-x))
 
     def sigmoid_derivative(self, x):
-        return self.sigmoid_derivative(x) * (self.sigmoid_function(x) - 1) 
+        return self.sigmoid_function(x) * (1 - self.sigmoid_function(x)) 
     
-    def train(self, inputs, expected_outputs, n_iterations = 10, learning_rate = 0):
-        pass
+    def fit(self, inputs, expected_outputs, n_iterations = 10, learning_rate = 0.5):
+        delta_weights = np.zeros((len(inputs[0]),len(inputs)))
+        for iteration in range(n_iterations):
+            print("Iteration #{}".format(iteration + 1))
+            x = np.dot(inputs, self.synapse_weights)
+            activation = self.sigmoid_function(x)
+            for i in range(len(inputs)):
+                #cost = (activation[i] - expected_outputs[i]) ** 2
+                cost_prime = 2 * (activation[i] - expected_outputs[i])
+                for j in range(4):
+                    delta_weights[j][i] = cost_prime * inputs[i][j] * self.sigmoid_derivative(x[i])
+                    
+    
+        delta_avg = np.array([np.average(delta_weights, axis = 1)]).T
+        self.synapse_weights = self.synapse_weights - delta_avg * learning_rate
+
+    def predict(self, inputs):
+        return self.sigmoid_function(np.dot(inputs, self.synapse_weights))
+
+
+
